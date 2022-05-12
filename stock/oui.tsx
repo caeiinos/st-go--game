ba93df5d313c38d1aa20d5ae7338278da7f87879
Jean De Roy <jeanderoy2001@gmail.com>
2022-05-12 18:36:25 +0200

    stock add

---
+++ stock/oui.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="oui" tilewidth="8" tileheight="8" tilecount="1400" columns="28">
+ <image source="../../2eme_2eme/tfa/truc/oui.png" width="228" height="404"/>
+</tileset>
