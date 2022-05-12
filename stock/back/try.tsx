54eddc9beef9b1cfbc2af957f42de0bf6c8b6373
caeiinos <jeanderoy2001@gmail.com>
2022-05-12 22:12:53 +0200

    new back

---
+++ stock/back/try.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="try" tilewidth="8" tileheight="8" tilecount="2160" columns="30">
+ <image source="asset/test.png" width="240" height="576"/>
+</tileset>
