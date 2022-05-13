00ba79fd8862c2aa3ee58cc9a842d8c72c314225
caeiinos <jeanderoy2001@gmail.com>
2022-05-13 11:46:01 +0200

    post wera

---
+++ stock/cbon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="cbon" tilewidth="8" tileheight="8" tilecount="4032" columns="64">
+ <image source="dededede.png" width="512" height="510"/>
+</tileset>
